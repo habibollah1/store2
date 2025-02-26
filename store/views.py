a4457e318ab05d6980527465dece7f96e72f4c4c
habib allah <haabibkhojamli@gamil.com>
2025-02-27 00:40:22 +0330

    start project with api rest framework and new information in database.

---
+++ store/views.py
@@ -0,0 +1,12 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view()
+def product_list(request):
+    return Response('Hello')
+
+
+@api_view()
+def product_detail(request, id):
+    return Response(id)
